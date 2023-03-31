3bdf56ab35c92b40d4a1809faa2c840e12e3bb2a
Miles <miles.collins@ymail.com>
2023-03-31 16:04:11 -0600

    Can't create on non collabed album

--- PostIt/server/services/PicturesService.js
+++ PostIt/server/services/PicturesService.js
@@ -1,7 +1,17 @@
 import { dbContext } from "../db/DbContext";
+import { Forbidden } from "../utils/Errors";
 
 class PicturesService {
   async Create(pictureData) {
+    let albumId = pictureData.albumId;
+    let accountId = pictureData.accountId;
+    let collaborator = dbContext.AlbumMember.findOne({
+      albumId: albumId,
+      accountId: accountId,
+    });
+    if (collaborator == null) {
+      throw new Forbidden(`You are not a collaborator on this Album.`);
+    }
     let picture = await dbContext.Picture.create(pictureData);
     await picture.populate("creator");
     await picture.populate("album");
